6596ece977defaf8acf5fff4060da2f0ab49d84e
Chandra Shekhar Joshi <cj.github@gmail.com>
2017-08-22 09:07:45 +0530

    React app state example

--- app/reactapp/component/hello.component.js
+++ app/reactapp/component/hello.component.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react'
 export class HelloComponent extends Component{
     constructor(props){
         super(props);
-        this.state = {"name": "React App works with your superbuild"}
+        this.state = {name: "React App works with your superbuild",myName:""}
     }
     render(){
         return (
@@ -11,8 +11,14 @@ export class HelloComponent extends Component{
                 <h3>
                     {this.state.name}
                 </h3>
-                <input type="text" placeholder="Type your name here"
-                       className="form-control" onChange={event=>this.setState({myName : event.target.value})} />
+                <input type="text"
+                       placeholder="Type your name here"
+                       className="form-control"
+                       onChange={event=>this.setState({myName : event.target.value})}
+                       value={this.state.myName}/>
+                {/*Controlled component. Value of the input doesn't change when user types in.
+                Component refreshes when user start typing, now when component re-renders,
+                state gets new value and we see new value.*/}
                 My name is : {this.state.myName}
             </div>
         )
